e594abc87953576015331d0d2e583f0e569fc7ec
Nicky van Urk <nickyvanurk@gmx.com>
2019-06-13 21:21:32 +0200

    Add proverb exercise

---
+++ proverb/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    if list.is_empty() { return String::new(); }
+
+    let mut proverb = String::new();
+    let mut last_word = list[0];
+
+    for item in list.iter().skip(1) {
+        proverb += format!(
+            "For want of a {} the {} was lost.\n",
+            last_word, item
+        ).as_str();
+
+        last_word = item;
+    }
+
+    proverb + format!("And all for the want of a {}.", list[0]).as_str()
+}
